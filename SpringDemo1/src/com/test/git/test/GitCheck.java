38c899f9b7ba3acdd1a90ac0c442c3e00d7bddf4
Sanu <sanu@testmail.com>
2017-03-23 01:09:20 +0530

    committing for 3rd tym in git test

--- SpringDemo1/src/com/test/git/test/GitCheck.java
+++ SpringDemo1/src/com/test/git/test/GitCheck.java
@@ -3,5 +3,6 @@ package com.test.git.test;
 public class GitCheck {
 public void checkGitTutorial(){
 	System.out.println("Hello checking GIT");
+	System.out.println("Here again committing in git");
 }
 }
