606a4990d3c80f33a34ece26e064a4419d9e6293
Sanu <sanu@testmail.com>
2017-03-23 01:40:35 +0530

    commit 6th

--- SpringDemo1/src/com/test/git/test/GitCheck.java
+++ SpringDemo1/src/com/test/git/test/GitCheck.java
@@ -1,15 +1,8 @@
 package com.test.git.test;
 
 public class GitCheck {
-<<<<<<< HEAD
-public void checkGitTutorial(){
-	System.out.println("Hello checking GIT");
-	System.out.println("Here again committing in git");
-}
-=======
-	public void checkGitTutorial(){
+	public void checkGitTutorial() {
 		System.out.println("Hello checking GIT");
-		System.out.println("Hello checking GIT Second Time");
+		System.out.println("Here again committing in git");
 	}
->>>>>>> 397122a6910ac00345a5f3e131b3269c953c6e4c
 }
